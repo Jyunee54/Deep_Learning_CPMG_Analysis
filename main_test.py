5d4147faa60b399003c70ff58619d3404a0d5691
Jyunee54 <jyunee54@gmail.com>
2025-05-27 17:15:20 +0900

    [setting] 파라미터 수정

--- main_test.py
+++ main_test.py
@@ -25,19 +25,17 @@ EXISTING_SPINS = 0
 EVALUATION_ALL = 0
 
 target_side_distance = 3000
-A_init  = 20000
-A_final = 40000
-A_step  = 500
-A_range = 500
-B_init  = 20000
+A_init  = -30000
+A_final = 30000
+A_step  = 200
+A_range = 200
+B_init  = 1500
 B_final = 50000
 noise_scale = 0.5
 zero_scale = 0.05
 SAVE_DIR_NAME = "./data/results/"
 is_CNN = 0
 is_remove_model_index = 0
-
-model_lists = get_AB_model_lists(A_init, A_final, A_step, A_range, B_init, B_final)
 
 # ✅ 핵심 실행부를 __main__ 블록으로 감쌈
 if __name__ == '__main__':
@@ -49,12 +47,14 @@ if __name__ == '__main__':
     if EVALUATION_ALL:
         EXISTING_SPINS = 0
         A_init, A_final, B_init, B_final, target_side_distance = -50000, 50000, 12000, 80000, 50
+        model_lists = get_AB_model_lists(A_init, A_final, A_step, A_range, B_init, B_final)
         args = (CUDA_DEVICE, N_PULSE, IMAGE_WIDTH, TIME_RANGE_32, EXISTING_SPINS, A_init, A_final,
                 A_step, A_range, B_init, B_final, noise_scale, SAVE_DIR_NAME, model_lists, target_side_distance, is_CNN, is_remove_model_index)
 
         hpc_model = HPC_Model(*args)
         total_A_lists, total_raw_pred_list, total_deno_pred_list = hpc_model.binary_classification_train()
-        predicted_periods = return_filtered_A_lists_wrt_pred(total_deno_pred_list[1,:])
+        predicted_periods = return_filtered_A_lists_wrt_pred(total_deno_pred_list[1,:], np.array(total_A_lists))
+        np.save(SAVE_DIR_NAME + "predicted_periods.npy", predicted_periods)
 
         regression_args = (CUDA_DEVICE, N_PULSE, IMAGE_WIDTH, TIME_RANGE_32, TIME_RANGE_256, EXISTING_SPINS, A_init, A_final,
                             A_step, A_range, B_init, B_final, noise_scale, SAVE_DIR_NAME, model_lists, target_side_distance, is_CNN)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         filtered_results = [res for res in regression_results if res[1] > 15000]  # res = [A, B] 형태라고 가정
         filtered_results = np.array(filtered_results)
 
-        save_path = './data/predicted_results_N32_B15000above.npy'
+        save_path = SAVE_DIR_NAME + 'predicted_results_N32_B15000above.npy'
         np.save(save_path, filtered_results)
         print(f"✅ B > 15000인 결과를 {save_path} 에 저장했습니다.")
 
@@ -74,7 +74,7 @@ if __name__ == '__main__':
         # regression_model.estimate_the_number_of_spins(A_lists)
         # regression_model.estimate_specific_AB_values(A_lists_with_the_number_of_spins)
         # 여기서 얻은 것 중에, B=15000보다 큰 리스트를 저장해놓음. --> 아래 경로로.
-        # np.load('./data/predicted_results_N32_B15000above.npy')
+        # np.load(SAVE_DIR_NAME + 'predicted_results_N32_B15000above.npy')
 
         EXISTING_SPINS = 1
         B_init, B_final = 6000, 12000
@@ -104,6 +104,7 @@ if __name__ == '__main__':
         regression_model.close_pool()
 
     else:
+        model_lists = get_AB_model_lists(A_init, A_final, A_step, A_range, B_init, B_final)
         if N_PULSE==32:
             args = (CUDA_DEVICE, N_PULSE, IMAGE_WIDTH, TIME_RANGE_32, EXISTING_SPINS, A_init, A_final,
                     A_step, A_range, B_init, B_final, noise_scale, SAVE_DIR_NAME, model_lists, target_side_distance, is_CNN, is_remove_model_index)
@@ -116,7 +117,7 @@ if __name__ == '__main__':
         print('Total computational time:', time.time() - tic)
 
         predicted_periods = return_filtered_A_lists_wrt_pred(np.array(total_deno_pred_list[1,:]), np.array(total_A_lists), 0.8)
-        # predicted_periods = np.load('/home/sonic/Coding/Git/Paper_git_repo/Deep_Learning_CPMG_Analysis/data/models/predicted_periods.npy')
+        # predicted_periods = np.load(SAVE_DIR_NAME + 'predicted_periods.npy')
         hpc_model.close_pool()
 
         # 디버깅
@@ -127,13 +128,22 @@ if __name__ == '__main__':
         regression_args = (CUDA_DEVICE, N_PULSE, IMAGE_WIDTH, TIME_RANGE_32, TIME_RANGE_256, EXISTING_SPINS, A_init, A_final,
                             A_step, A_range, B_init, B_final, zero_scale, noise_scale, SAVE_DIR_NAME, model_lists, target_side_distance, is_CNN)
         regression_model = Regression_Model(*regression_args)
+        regression_results = regression_model.estimate_specific_AB_values(predicted_periods)
 
-        try:
-            regression_results = regression_model.estimate_specific_AB_values(predicted_periods)
-            with open('./data/results/regression_results.pkl', 'wb') as f:
-                pickle.dump(regression_results, f)
-        finally:
-            regression_model.close_pool()
+        # B 값이 15000 이상인 하이퍼핀 파라미터만 필터링해서 저장
+        filtered_results = [res for res in regression_results if res[1] > 15000]  # res = [A, B] 형태라고 가정
+        filtered_results = np.array(filtered_results)
+
+        save_path = SAVE_DIR_NAME + 'predicted_results_N32_B15000above.npy'
+        np.save(save_path, filtered_results)
+        print(f"✅ B > 15000인 결과를 {save_path} 에 저장했습니다.")
+
+        # try:
+        #     regression_results = regression_model.estimate_specific_AB_values(predicted_periods)
+        #     with open('./data/results/regression_results.pkl', 'wb') as f:
+        #         pickle.dump(regression_results, f)
+        # finally:
+        #     regression_model.close_pool()
 
 
     # 테스트 및 디버깅용 수동 실행 코드
@@ -149,4 +159,4 @@ if __name__ == '__main__':
     # regression_args = (CUDA_DEVICE, N_PULSE, IMAGE_WIDTH, TIME_RANGE_32, TIME_RANGE_256, EXISTING_SPINS, A_init, A_final, A_step, A_range, B_init, B_final, zero_scale, noise_scale, SAVE_DIR_NAME, model_lists, target_side_distance, is_CNN)
     # regression_model = Regression_Model(*regression_args)
     # regression_results = regression_model.estimate_specific_AB_values(predicted_periods)
-    # np.save('/home/sonic/Coding/Git/Paper_git_repo/Deep_Learning_CPMG_Analysis/data/models/regression_results.npy', regression_results)
+    # np.save(SAVE_DIR_NAME + 'regression_results.npy', regression_results)
